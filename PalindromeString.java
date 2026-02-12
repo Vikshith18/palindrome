b26b2c017ff37b2689d99880a43b36126ed8ab8d
Vikshith18 <vikshithreddy18@gmail.com>
2026-02-12 18:37:07 +0530

    Palindrome String

---
+++ PalindromeString.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        
+        int left = 0;
+        int right = str.length() - 1;
+        boolean isPalindrome = true;
+        
+        while(left < right) {
+            if(str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        
+        if(isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        
+        sc.close();
+    }
+}
